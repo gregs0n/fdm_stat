0fab00935b6529b7e3d43a38cdb71056f8dda9cf
gregs0n <volkovpy@mpei.ru>
2023-11-27 14:57:20 +0300

    add FDM class & __init__ headers

---
+++ scheme.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class FDM:
+    def __init__(self,
+                 F: np.ndarray,
+                 G: np.ndarray,
+                 limits: list[np.float64, np.float64],
+                 n: int):
+        pass
