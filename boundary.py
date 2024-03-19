48b5d0f2a77027838e8dbcafe5177310c62dc853
gregs0n <volkovpy@mpei.ru>
2024-03-19 11:05:26 +0300

    rewrite tests & format code

--- boundary.py
+++ boundary.py
@@ -7,21 +7,31 @@ stef_bolc = 5.67036713
 
 HeatStream = lambda t: stef_bolc * np.power(t, 4)
 
+
 def GetFunc(f_num, g_num):
     ## returns f(x, y) and list of 4 g(t) NORMED
-    tmax = 6.0 #500K, actually
+    tmax = 6.0  # 500K, actually
     tmin = 3.0
     coef = tmax - tmin
     d = tmin
     f = lambda x, y: 0.0
     g = [
-        lambda t: HeatStream(d + coef * np.sin(np.pi * t / 0.3)) if 0.0 <= t <= 0.3 else HeatStream(tmin),
-        #lambda t: HeatStream(tmax),
+        lambda t: (
+            HeatStream(d + coef * np.sin(np.pi * t / 0.3))
+            if 0.0 <= t <= 0.3
+            else HeatStream(tmin)
+        ),
+        # lambda t: HeatStream(tmax),
         lambda t: HeatStream(tmin),
-        lambda t: HeatStream(tmin),#HeatStream(d + coef * np.sin(np.pi * (1.0 - t) / 0.3)) if 0.7 <= t <= 1.0 else HeatStream(tmin),
+        lambda t: (
+            HeatStream(d + coef * np.sin(np.pi * (1.0 - t) / 0.3))
+            if 0.7 <= t <= 1.0
+            else HeatStream(tmin)
+        ),
         lambda t: HeatStream(tmin),
     ]
     return f, g
+
 
 def GetBoundary(test: Test) -> list:
     bnd, material, cells, limits = list(test._asdict().values())[1:]
@@ -36,21 +46,33 @@ def GetBoundary(test: Test) -> list:
         x1 = i * h
         for j in range(cells):
             x2 = j * h
-            F[i, j], _ = nquad(f, [[x1, x1+h], [x2, x2+h]])
+            F[i, j], _ = nquad(f, [[x1, x1 + h], [x2, x2 + h]])
 
     for k in range(1, cells - 1):
         x = k * h
-        G[ k,  0], _ = quad(g[0], x, x + h)
-        G[-1,  k], _ = quad(g[1], x, x + h)
-        G[ k, -1], _ = quad(g[2], x, x + h)
-        G[ 0,  k], _ = quad(g[3], x, x + h)
+        G[k, 0], _ = quad(g[0], x, x + h)
+        G[-1, k], _ = quad(g[1], x, x + h)
+        G[k, -1], _ = quad(g[2], x, x + h)
+        G[0, k], _ = quad(g[3], x, x + h)
 
-    G[ 0,  0] = 0.5 * (quad(g[0], limits[0], limits[0]+h)[0] + quad(g[3], limits[0], limits[0]+h)[0])
-    G[-1,  0] = 0.5 * (quad(g[0], limits[1]-h, limits[1])[0] + quad(g[1], limits[0], limits[0]+h)[0])
-    G[-1, -1] = 0.5 * (quad(g[1], limits[1]-h, limits[1])[0] + quad(g[2], limits[1]-h, limits[1])[0])
-    G[ 0, -1] = 0.5 * (quad(g[2], limits[0], limits[0]+h)[0] + quad(g[3], limits[1]-h, limits[1])[0])
+    G[0, 0] = 0.5 * (
+        quad(g[0], limits[0], limits[0] + h)[0]
+        + quad(g[3], limits[0], limits[0] + h)[0]
+    )
+    G[-1, 0] = 0.5 * (
+        quad(g[0], limits[1] - h, limits[1])[0]
+        + quad(g[1], limits[0], limits[0] + h)[0]
+    )
+    G[-1, -1] = 0.5 * (
+        quad(g[1], limits[1] - h, limits[1])[0]
+        + quad(g[2], limits[1] - h, limits[1])[0]
+    )
+    G[0, -1] = 0.5 * (
+        quad(g[2], limits[0], limits[0] + h)[0]
+        + quad(g[3], limits[1] - h, limits[1])[0]
+    )
 
-    F *= 1.0/h**2
-    G *= 1.0/h
+    F *= 1.0 / h**2
+    G *= 1.0 / h
 
     return [F, G]
--- tests.py
+++ tests.py
@@ -1,41 +1,43 @@
+#!/home/gregs0n/venvs/numpy-venv/bin/python3
 
 from enviroment import *
 from draw import *
-from scheme import *
+from fdm_scheme import *
+from sdm_scheme import *
 
 import numpy as np
-from scipy.integrate import quad
+
 
 def main():
     start = 10
     finish = 45
     tccs = [1, 5, 10, 20, 50, 100, 200]
     errs = [[] for _ in tccs]
-    for cells in range(start, finish+1):
+    for cells in range(start, finish + 1):
         material = Material("template", 5.12)
         test = Test(0, [0, 0], material, cells)
         sol_exact = [np.load(f"two_sin/{tcc}/{cells}.npy") for tcc in tccs]
-        #for i in range(len(sol_exact)):
-        #    drawHeatmap(sol_exact[i], [0, 1], f"two_sin/{cells}_{tccs[i]}", zlim=(300, 600), show_plot=0)
-        #    print(f"{cells}:{tccs[i]} ready")
-        #continue
         F, G = GetBoundary(test)
-        fdm = FDM(F, G, cells, 1.0/cells, [0.0, 1.0])
-        fdm._solve()
-        sol_fdm = fdm.Solve_fdm()
+        #fdm = FDM(F, G, cells, 1.0 / cells, [0.0, 1.0], material)
+        #sol_fdm = fdm.solve()
         for i in range(len(tccs)):
-            #sol_sdm = fdm.Solve_sdm(tccs[i])
-            err_loc = np.abs(sol_exact[i] - sol_fdm).max()
-            errs[i].append(err_loc.max()/(sol_exact[i].max() - sol_exact[i].min()))
-        print(cells, ": ready")
-    
+            sdm = SDM(
+                F, G, cells, 1.0 / cells, [0.0, 1.0], Material("template", tccs[i])
+            )
+            sol_sdm = sdm.solve(1.0e-3, 300.0 * np.ones_like(F))
+            err_loc = np.abs(sol_exact[i] - sol_sdm).max()
+            errs[i].append(err_loc.max() / (sol_exact[i].max() - sol_exact[i].min()))
+        print(f"{cells} : ready |")
+
     draw1D(
         list(map(np.array, errs)),
         [start, finish],
         "errors",
         legends=list(map(lambda s: f"$\lambda$={s}", tccs)),
         yscale="log",
-        show_plot=0)
+        show_plot=1,
+    )
+
 
 if __name__ == "__main__":
     main()
